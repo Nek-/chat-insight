c16d208accfd3be73e54940880a98e5ce54056c5
Kevin Grenier <kevin.grenier@biig.fr>
2018-11-22 23:46:32 +0100

    Add back devs

--- back/nicknames.ts
+++ back/nicknames.ts
@@ -4,7 +4,6 @@ export interface BiiGBackDev {
   nickname: string;
 }
 
-
 export const devs: BiiGBackDev[] = [
   {
     prenom: 'keving',
@@ -15,5 +14,75 @@ export const devs: BiiGBackDev[] = [
     prenom: 'maximev',
     birthdate: '30-10',
     nickname: 'La super star'
-  }
+  },
+  {
+    prenom: 'mathieul',
+    birthdate: '01-06',
+    nickname: ''
+  },
+  {
+    prenom: 'louisa',
+    birthdate: '02-12',
+    nickname: ''
+  },
+  {
+    prenom: 'maximev',
+    birthdate: '30-10',
+    nickname: ''
+  },
+  {
+    prenom: 'alexist',
+    birthdate: '30-09',
+    nickname: ''
+  },
+  {
+    prenom: 'celinem',
+    birthdate: '04-08',
+    nickname: ''
+  },
+  {
+    prenom: 'dimitris',
+    birthdate: '13-04',
+    nickname: ''
+  },
+  {
+    prenom: 'elisabethb',
+    birthdate: '04-12',
+    nickname: ''
+  },
+  {
+    prenom: 'etiennec',
+    birthdate: '03-04',
+    nickname: ''
+  },
+  {
+    prenom: 'loicp',
+    birthdate: '24-02',
+    nickname: ''
+  },
+  {
+    prenom: 'markl',
+    birthdate: '23-12',
+    nickname: ''
+  },
+  {
+    prenom: 'matthieuc',
+    birthdate: '19-08',
+    nickname: ''
+  },
+  {
+    prenom: 'maximel',
+    birthdate: '02-04',
+    nickname: ''
+  },
+  {
+    prenom: 'valentinf',
+    birthdate: '01-08',
+    nickname: ''
+  },
+  {
+    prenom: 'yassinea',
+    birthdate: '05-06',
+    nickname: ''
+  },
 ];
